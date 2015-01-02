1a5f8cd263fe1ff2bccff066a57976a3313067ce
Rob J Goedman <goedman@mac.com>
2015-01-01 17:05:09 -0800

    Reorganized.

--- src/ClassicalLaminateTheory.jl
+++ src/ClassicalLaminateTheory.jl
@@ -1,5 +1,27 @@
 module ClassicalLaminateTheory
 
 # package code goes here
+using Compat
+
+include("clt.jl")
+
+if !isdefined(Main, :JULIA_SVG_BROWSER)
+  JULIA_SVG_BROWSER = ""
+  try
+    JULIA_SVG_BROWSER = ENV["JULIA_SVG_BROWSER"]
+  catch e
+    println("Environment variable JULIA_SVG_BROWSER not found.")
+    JULIA_SVG_BROWSER = ""
+  end
+end
+
+export
+  # From clt.jl
+  qmat,
+  qbarmat,
+  createLaminate!,
+  
+  # From BucklingOfPipes.jl
+  JULIA_SVG_BROWSER
 
 end # module
